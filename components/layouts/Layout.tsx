5cf681b35e7ceec67ad60bd44635424f119652dc
alan <alanarostegui@hotmail.com>
2022-05-19 23:59:16 -0500

    feat: add sidebar

--- components/layouts/Layout.tsx
+++ components/layouts/Layout.tsx
@@ -1,7 +1,7 @@
 import { FC, ReactNode } from "react";
 import Head from "next/head";
 import { Box } from "@mui/material";
-import { Navbar } from "../ui";
+import { Navbar, Sidebar } from "../ui";
 
 interface Props {
   title?: string;
@@ -19,7 +19,7 @@ export const Layout: FC<Props> = ({ title = "OpenJira", children }) => {
         <title>{title}</title>
       </Head>
       <Navbar />
-      {/* Sidebar */}
+      <Sidebar />
       <Box sx={{ padding: "10px 20px" }}>{children}</Box>
     </Box>
   );
---
+++ components/ui/Sidebar.tsx
@@ -0,0 +1,46 @@
+import { InboxOutlined, MailOutlineOutlined } from "@mui/icons-material";
+import {
+  Box,
+  Divider,
+  Drawer,
+  List,
+  ListItem,
+  ListItemIcon,
+  ListItemText,
+  Typography,
+} from "@mui/material";
+import React from "react";
+
+const menuItems: string[] = ["Inbox", "Starred", "Send email", "Drafts"];
+
+export const Sidebar = () => {
+  return (
+    <Drawer anchor="left" open={true} onClose={() => console.log("cerrando")}>
+      <Box sx={{ width: 250 }}>
+        <Typography variant="h4">Menú</Typography>
+        <List>
+          {menuItems.map((text, index) => (
+            <ListItem button key={index}>
+              <ListItemIcon>
+                {index % 2 ? <InboxOutlined /> : <MailOutlineOutlined />}
+              </ListItemIcon>
+              <ListItemText primary={text} />
+            </ListItem>
+          ))}
+        </List>
+        <Divider />
+        <List>
+          {menuItems.map((text, index) => (
+            <ListItem button key={index}>
+              <ListItemIcon>
+                {index % 2 ? <InboxOutlined /> : <MailOutlineOutlined />}
+              </ListItemIcon>
+              <ListItemText primary={text} />
+            </ListItem>
+          ))}
+        </List>
+      </Box>
+      <Box sx={{ padding: "5px 10px" }}></Box>
+    </Drawer>
+  );
+};
