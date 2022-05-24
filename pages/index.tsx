ff937ba412947f12e0d610b9eb0e8ceae076e246
alan <alanarostegui@hotmail.com>
2022-05-23 23:15:07 -0500

    feat: main design columns

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,13 +1,31 @@
-import { Typography } from "@mui/material";
+import { Card, CardContent, CardHeader, Grid, Typography } from "@mui/material";
 import type { NextPage } from "next";
 import { Layout } from "../components/layouts";
 
 const Home: NextPage = () => {
   return (
-    <Layout>
-      <Typography variant="h1" color="primary">
-        Hola jaja
-      </Typography>
+    <Layout title="Home - OpenJira">
+      <Grid container spacing={2}>
+        <Grid item xs={12} sm={4}>
+          <Card sx={{ height: "calc(100vh - 100px" }}>
+            <CardHeader title="Pendientes" />
+            <CardContent>
+              {/* Add new entry */}
+              {/* list all entries */}
+            </CardContent>
+          </Card>
+        </Grid>
+        <Grid item xs={12} sm={4}>
+          <Card sx={{ height: "calc(100vh - 100px" }}>
+            <CardHeader title="En Progreso" />
+          </Card>
+        </Grid>
+        <Grid item xs={12} sm={4}>
+          <Card sx={{ height: "calc(100vh - 100px" }}>
+            <CardHeader title="Contempladas" />
+          </Card>
+        </Grid>
+      </Grid>
     </Layout>
   );
 };
