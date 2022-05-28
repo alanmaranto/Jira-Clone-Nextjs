0b30f3ee2cb4fc2696f051995f1f60fc577c932c
alan <alanarostegui@hotmail.com>
2022-05-28 15:36:30 -0500

    feat: creating mongoose schema

---
+++ models/Entry.ts
@@ -0,0 +1,20 @@
+import mongoose, { Model, Schema } from "mongoose";
+import { Entry } from '../interfaces';
+
+interface IEntry extends Entry {}
+
+const entrySchema = new Schema({
+  description: { type: String, required: true },
+  createdAt: { type: Number },
+  status: {
+    type: String,
+    enum: {
+      message: "{VALUE} is not a valid status",
+      values: ["pending", "in-progress", "finished"],
+    },
+  },
+});
+
+const EntryModel: Model<IEntry> = mongoose.models.Entry || mongoose.model("Entry", entrySchema);
+
+export default EntryModel;
