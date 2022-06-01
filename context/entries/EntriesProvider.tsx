b96fe1bb5167b825ff57b0e34ae8187cb910fd63
alan <alanarostegui@hotmail.com>
2022-05-31 23:34:10 -0500

    feat: update entry (frontend)

--- context/entries/EntriesProvider.tsx
+++ context/entries/EntriesProvider.tsx
@@ -31,8 +31,17 @@ export const EntriesProvider: FC<PropsWithChildren<Props>> = ({ children }) => {
     dispatch({ type: "Entry - Add-Entry", payload: data });
   };
 
-  const updateEntry = (entry: Entry) => {
-    dispatch({ type: "Entry - Entry-Updated", payload: entry });
+  const updateEntry = async ({ _id, description, status }: Entry) => {
+    try {
+      const { data } = await entriesApi.put<Entry>(`/entries/${_id}`, {
+        description,
+        status,
+      });
+
+      dispatch({ type: "Entry - Entry-Updated", payload: data });
+    } catch (error) {
+      console.log(error);
+    }
   };
 
   return (
