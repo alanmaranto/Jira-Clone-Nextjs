43eb17e7f79ef30c057c703c732c1c93ce5a60c6
alan <alanarostegui@hotmail.com>
2022-07-10 01:39:10 -0500

    fix: disconnected from mongo

--- database/db.ts
+++ database/db.ts
@@ -37,5 +37,6 @@ export const disconnect = async () => {
   if (mongoConnection.isConnected === 0) return;
 
   await mongoose.disconnect();
+  mongoConnection.isConnected = 0;
   console.log("desconectado de mongodb");
 };
