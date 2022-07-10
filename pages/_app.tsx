9933eb6bba14a230657b376359c96426cf663898
alan <alanarostegui@hotmail.com>
2022-07-10 00:33:35 -0500

    feat: install notistack and add globally

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,21 +1,23 @@
 import "../styles/globals.css";
 import type { AppProps } from "next/app";
 import { createTheme, ThemeProvider, CssBaseline } from "@mui/material";
-
+import { SnackbarProvider } from "notistack";
 import { lightTheme, darkTheme } from "../themes";
 import { UIProvider } from "../context/ui";
 import { EntriesProvider } from "../context/entries";
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
-    <EntriesProvider>
-      <UIProvider>
-        <ThemeProvider theme={darkTheme}>
-          <CssBaseline />
-          <Component {...pageProps} />
-        </ThemeProvider>
-      </UIProvider>
-    </EntriesProvider>
+    <SnackbarProvider maxSnack={3}>
+      <EntriesProvider>
+        <UIProvider>
+          <ThemeProvider theme={darkTheme}>
+            <CssBaseline />
+            <Component {...pageProps} />
+          </ThemeProvider>
+        </UIProvider>
+      </EntriesProvider>
+    </SnackbarProvider>
   );
 }
 
